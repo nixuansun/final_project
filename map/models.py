f03735eba934565f89aa7696da33a1a1d4e69b3d
Sophia Song <ss6125@columbia.edu>
2021-04-14 14:58:19 +0000

    Update

--- map/models.py
+++ map/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+class SightingsSquirrelData(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    unique_squirrel_id = models.CharField(unique=True, max_length=200)
+    shift = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    age = models.CharField(max_length=200, blank=True, null=True)
+    primary_fur_color = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    specific_location = models.CharField(max_length=200, blank=True, null=True)
+    running = models.BooleanField(blank=True, null=True)
+    chasing = models.BooleanField(blank=True, null=True)
+    climbing = models.BooleanField(blank=True, null=True)
+    eating = models.BooleanField(blank=True, null=True)
+    foraging = models.BooleanField(blank=True, null=True)
+    other_activities = models.CharField(max_length=200, blank=True, null=True)
+    kuks = models.BooleanField(blank=True, null=True)
+    quaas = models.BooleanField(blank=True, null=True)
+    moans = models.BooleanField(blank=True, null=True)
+    tail_flags = models.BooleanField(blank=True, null=True)
+    tail_twitches = models.BooleanField(blank=True, null=True)
+    approaches = models.BooleanField(blank=True, null=True)
+    indifferent = models.BooleanField(blank=True, null=True)
+    runs_from = models.BooleanField(blank=True, null=True)
 # Create your models here.
--- map/views.py
+++ map/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from sightings.models import squirrel_data
 
-# Create your views here.
+def map(request):
+    map_squirrel = squirrel_data.objects.all()[:100]
+    return render(request,'map/map.html',{"map_squirrel":map_squirrel})
+.
